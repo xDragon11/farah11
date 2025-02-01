e5666a7834c19a82f5c529c2453d6ed82e789497
Farah <hishammshmsh9@gmail.com>
2025-02-01 03:17:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, request, redirect, url_for, jsonify, send_file
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
-# لوحة اللعب
 board = [[" " for _ in range(3)] for _ in range(3)]
 players = ["X", "O"]
 turn = 0
 
-# دالة للتحقق من الفوز
 def check_winner(board, player):
     for i in range(3):
         if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
@@ -16,16 +14,13 @@ def check_winner(board, player):
         return True
     return False
 
-# دالة للتحقق من التعادل
 def is_full(board):
     return all(board[i][j] != " " for i in range(3) for j in range(3))
 
-# الصفحة الرئيسية
 @app.route('/')
 def index():
-    return send_file('index.html')
+    return render_template('index.html', board=board, turn=turn)
 
-# دالة لمعالجة الحركات
 @app.route('/move', methods=['POST'])
 def move():
     global turn
@@ -42,17 +37,10 @@ def move():
         turn += 1
     return jsonify({'redirect': url_for('index')})
 
-# صفحة نهاية اللعبة
 @app.route('/game_over/<winner>')
 def game_over(winner):
-    return send_file('game_over.html')
+    return render_template('game_over.html', winner=winner)
 
-# ملف CSS
-@app.route('/style.css')
-def style():
-    return send_file('style.css')
-
-# إعادة تعيين اللعبة
 @app.route('/reset')
 def reset():
     global board, turn
